60e3d9cc06d9658399c67c1b3bf470559c693cdf
Davos DeHoyos <44310581+YungDouble@users.noreply.github.com>
2024-10-25 13:28:55 -0500

    Update LookForCheckOutCheckInDiscrepancies.sql
    changes for OXF docDetails discrepancies

--- LookForCheckOutCheckInDiscrepancies.sql
+++ LookForCheckOutCheckInDiscrepancies.sql
@@ -1,37 +1,31 @@
+USE yfOXF;
+
 WITH CheckInOutCount AS (
-	SELECT
-		bld.BoxLevelDetailsId,
-		SUM(CASE WHEN mat.AttrName = 'CheckIn' THEN 1 ELSE 0 END) AS CheckInCount,
-		SUM(CASE WHEN mat.AttrName = 'CheckOut' THEN 1 ELSE 0 END) AS CheckOutCount
-	FROM
-		[yfALD].[dbo].[tBoxLevelActivityDetails] bld
-		JOIN [dbo].[mAcctIdentity] mat
-			ON bld.DocOperationId = mat.[Id]
-	WHERE
-		bld.DocOperationId IN (28,29)
-	GROUP BY
-		bld.BoxLevelDetailsId
+    SELECT
+        tad.DocDetailsId,
+        MIN(tad.ID) AS ID,  -- Get the minimum ID to include in the final results (or you can choose MAX or another appropriate aggregation)
+        MIN(tad.DocOperationId) AS DocOperationId,  -- Include DocOperationId if needed for the result display
+        SUM(CASE WHEN mat.AttrName = 'CheckIn' THEN 1 ELSE 0 END) AS CheckInCount,
+        SUM(CASE WHEN mat.AttrName = 'Cancel CheckOut' THEN 1 ELSE 0 END) AS CancelCheckOutCount,
+        SUM(CASE WHEN mat.AttrName = 'CheckOut' THEN 1 ELSE 0 END) AS CheckOutCount
+    FROM
+        [yfOXF].[dbo].[tDocActivityDetails] tad
+        JOIN [dbo].[mAcctIdentity] mat
+            ON tad.DocOperationId = mat.[Id]
+    WHERE
+        tad.DocOperationId IN (27, 28, 29)
+    GROUP BY
+        tad.DocDetailsId
 )
-SELECT * 
+SELECT 
+    ID,
+    DocDetailsId,
+    DocOperationId,
+    CheckInCount,
+    CancelCheckOutCount,
+    CheckOutCount
 FROM CheckInOutCount
-WHERE (CheckInCount = 1 AND CheckOutCount = 0)
-	OR (CheckInCount = 0 AND CheckOutCount = 1)
-ORDER BY BoxLevelDetailsId;
-
---SELECT * FROM dbo.tBoxLevelActivityDetails
---WHERE BoxLevelDetailsId IN (
---) AND DocOperationId = 29
-
---DELETE FROM dbo.tBoxLevelActivityDetails
---WHERE [Id] IN (
---1772,1789,1825,1850,1883,1907,1915,1929)
---AND BoxLevelDetailsId IN (
---134
---,127
---,138
---,130
---,152
---,182
---,148
---,116
---) AND DocOperationId = 29
+WHERE 
+    (CheckInCount + CancelCheckOutCount) <> CheckOutCount
+    AND CheckOutCount > 0  -- Exclude cases where there are no check-outs
+ORDER BY DocDetailsId;
