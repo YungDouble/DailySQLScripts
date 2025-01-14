0dac5cba7886977368ceea45e8c3ce912535afd7
Davos DeHoyos <44310581+YungDouble@users.noreply.github.com>
2025-01-14 11:44:11 -0600

    Update DataMigrator-DatabaseCSV_format.sql
    adjusted for SAG SPED

--- DataMigrator-DatabaseCSV_format.sql
+++ DataMigrator-DatabaseCSV_format.sql
@@ -1,4 +1,4 @@
-USE yfAES
+USE yfSAG
 GO
 
 SELECT TOP (100000) 
@@ -21,5 +21,5 @@ SELECT TOP (100000)
       ISNULL([Alias], '') AS Alias,   -- Replace NULL with empty string
       ISNULL([ScanType], '') AS ScanType, -- Replace NULL with empty string
       ISNULL([InsertUpdateFlag], '') AS InsertUpdateFlag -- Replace NULL with empty string
-  FROM [yfBMH].[dbo].[tPersonData]
-  WHERE RecordSeriesId = 3;
+  FROM [yfSAG].[dbo].[tPersonData]
+  WHERE RecordSeriesId = 2;
